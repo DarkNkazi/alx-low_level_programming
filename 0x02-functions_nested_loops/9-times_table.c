8d9b13a40e32a296503d103a832b1a3cba3daeb1
Anele Ngxongo <anelengxongo@yahoo.com>
2023-03-21 18:35:10 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,40 +7,26 @@
  */
 void times_table(void)
 {
-	int a, j, c;
+	int a, b, c;
 
 	for (a = 0; a <= 9; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		_putchar('0');
+		for (b = 1; b <= 9; b++)
 		{
 			c = a * b;
-
-			if ((c / 10) == 0)
+			_putchar(',');
+			_putchar(' ');
+			if (c <= 9)
 			{
-				else if (b != 0)
-				{
-					_putchar(' ');
-					_putchar(c + '0');
-				}
-				else if (b == 9)
-				{
-					continue;
-					_putchar(',');
-					_putchar(' ');
-				}
+				_putchar(' ');
+				_putchar(c + '0');
 			}
 			else
 			{
 				_putchar((c / 10) + '0');
-				_putchar((c % 10) + '0');
-				if (b == 9)
-				{
-					continue;
-					_putchar(',');
-					_putchar(' ');
-				}
+				_putchar((c % 10) + 'o');
 			}
 		}
-		_putchar('\n');
 	}
 }
