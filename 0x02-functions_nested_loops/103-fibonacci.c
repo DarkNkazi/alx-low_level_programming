5d7eb4a0b0efe6e87f7f0fecdfa8c4c0fbc5882f
Anele Ngxongo <anelengxongo@yahoo.com>
2023-03-21 20:45:31 -0700

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Print the sum of even Fibonacci numbers
+ *
+ * Return: Nothing
+ */
+int main(void)
+{
+	int a = 0;
+	long b = 1;
+	long c = 2;
+	long d = k;
+
+	while
+	{
+		c += b;
+		if (c % 2 == 0)
+			d += c;
+		b = c - b;
+		++a;
+	}
+	printf("%ld\n", d);
+	return (0);
+}
